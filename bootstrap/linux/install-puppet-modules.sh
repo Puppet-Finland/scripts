7dc2984b3683bff6a10c2aaef001f761cdb1e8f7
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2020-03-03 10:25:40 +0200

    Create fully functional modules directory in install-puppet-modules.sh
    The link to the root module as well as the dependency modules are needed
    or "puppet apply" will fail.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- bootstrap/linux/install-puppet-modules.sh
+++ bootstrap/linux/install-puppet-modules.sh
@@ -8,28 +8,39 @@
 set -e
 
 usage() {
-    echo "Usage: install-puppet-modules.sh [-f <puppetfile>] [-m <moduledir>] [-h]"
+    echo "Usage: install-puppet-modules.sh [-n <modulename>] [-f <puppetfile>] [-r <moduleroot>] [-m <moduledir>] [-h]"
     echo
     echo "Options:"
+    echo "    -n    module for which these dependencies are installed (no default value)"
     echo "    -f    path to Puppetfile (default: /vagrant/Puppetfile)"
+    echo "    -r    path to module root (default: /vagrant)"
     echo "    -m    the directory to install modules to (default: /vagrant/modules)"
     echo "    -h    show this help"
     echo
     exit 2
 }
 
-# Default settings
+# Default settings suitable for most Vagrant environments
+MODULENAME=""
 PUPPETFILE="/vagrant/Puppetfile"
+MODULEROOT="/vagrant"
 MODULEDIR="/vagrant/modules"
 
-while getopts 'p:m:h' arg
+while getopts 'n:p:r:m:h' arg
 do
   case $arg in
+    n) MODULENAME=$OPTARG ;;
     p) PUPPETFILE=$OPTARG ;;
+    r) MODULEROOT=$OPTARG ;;
     m) MODULEDIR=$OPTARG ;;
     h) usage ;;
   esac
 done
+
+if [ "$MODULENAME" = "" ]; then
+    echo "ERROR: root module name must be given with -n <name>!"
+    exit 1
+fi
 
 export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin
 
@@ -50,7 +61,13 @@ install_modules() {
     r10k puppetfile install -v --puppetfile=$PUPPETFILE --moduledir=$MODULEDIR --force
 }
 
+add_root_module_link() {
+    MODULEROOT=$1
+    MODULENAME=$2
+    ln -s $MODULEROOT $MODULEDIR/$MODULENAME
+}
 
 install_git
 install_r10k
 install_modules $PUPPETFILE $MODULEDIR
+add_root_module_link $MODULEROOT $MODULENAME
